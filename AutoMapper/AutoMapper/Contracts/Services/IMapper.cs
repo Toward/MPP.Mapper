704bc96230ac4b2eece050d4fc8e0e96cdb02eea
Toward <kirillsmolovskii@outlook.com>
2016-11-21 19:09:03 +0300

    Solution init

---
+++ AutoMapper/AutoMapper/Contracts/Services/IMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace AutoMapper.Contracts.Services
+{
+    public interface IMapper
+    {
+        TDestination Map<TSource, TDestination>(TSource source) where TDestination : new();
+    }
+}
